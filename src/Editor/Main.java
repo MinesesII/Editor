38c89a4acc034b83e9a6bd4227cd7f0e41a0d690
MinesesII <sonicyugi@gmail.com>
2014-08-02 17:09:00 +0530

    ADD : background + socle

--- src/Editor/Main.java
+++ src/Editor/Main.java
@@ -9,13 +9,17 @@ import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.Vector2f;
+import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
+import com.jme3.ui.Picture;
 
 public class Main extends SimpleApplication 
 { 
 	Vector2f oldposition;
 	private static Main editor;
+	private Node theObject = new Node ("The object");
 
 	public static void main(String[] args)
 	{
@@ -27,18 +31,55 @@ public class Main extends SimpleApplication
 	public void simpleInitApp() 
 	{
 		editor = this;
+		makeBackground();
 		createDefaultObject();
+		createBase();
 		initializeControls(); 
 		oldposition = inputManager.getCursorPosition().clone();
+	}
+	
+	private void makeBackground()
+	{
+		Picture p = new Picture("background");
+		p.setImage(assetManager, "sky.png", false);
+		p.setWidth(settings.getWidth());
+		p.setHeight(settings.getHeight());
+		p.setPosition(0, 0);
+		ViewPort pv = renderManager.createPreView("background", cam);
+		pv.setClearFlags(true, true, true);
+		pv.attachScene(p);
+		p.updateGeometricState();
+		viewPort.setClearFlags(false, true, true);
+	}
+	
+	private void createBase()
+	{
+		Node socle = new Node("socle");
+		Geometry base = new Geometry("Box", new Box((float)0.6,(float) 0.1, (float)0.6));  
+		Geometry base1 = new Geometry("Box", new Box((float)0.2,(float) 1, (float)0.2));  
+		Geometry base2 = new Geometry("Box", new Box((float)0.5,(float) 0.1, (float)0.5));  
+		Material mat = new Material(assetManager,"Common/MatDefs/Misc/Unshaded.j3md");
+		mat.setTexture("ColorMap", assetManager.loadTexture("Socle.png"));
+		base.setMaterial(mat); 
+		base1.setMaterial(mat);  
+		base2.setMaterial(mat); 
+		socle.attachChild(base); 
+		socle.attachChild(base1);
+		socle.attachChild(base2);
+		base.setLocalTranslation(0,(float) -3.8, 0);
+		base1.setLocalTranslation(0, (float)-2.8, 0);
+		base2.setLocalTranslation(0, (float)-1.8, 0);
+		rootNode.attachChild(socle);
 	}
 
 	private void createDefaultObject()
 	{
-		Geometry theObject = new Geometry("Box", new Box(1, 1, 1));  // create cube geometry from the shape
-		Material mat = new Material(assetManager,"Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
-		mat.setTexture("ColorMap", assetManager.loadTexture("test.png")); // set texture
-		theObject.setMaterial(mat);                   // set the cube's material
-		rootNode.attachChild(theObject);              // make the cube appear in the scene
+		Geometry model = new Geometry("Box", new Box(1, 1, 1));  
+		Material mat = new Material(assetManager,"Common/MatDefs/Misc/Unshaded.j3md");  
+		mat.setTexture("ColorMap", assetManager.loadTexture("Glowstone.png"));
+		model.setMaterial(mat);              
+		theObject.attachChild(model);
+		rootNode.attachChild(theObject);              
 	}
 
 	private void initializeControls()
@@ -64,23 +105,23 @@ public class Main extends SimpleApplication
 				Vector2f newposition = inputManager.getCursorPosition();
 				if(newposition.getX()<oldposition.getX())
 				{
-					rootNode.rotate(0,(float)-0.03, 0);
+					theObject.rotate(0,(float)-0.03, 0);
 				}
 				else if(newposition.getX()>oldposition.getX())
 				{
-					rootNode.rotate(0,(float)0.03, 0);
+					theObject.rotate(0,(float)0.03, 0);
 				}
 				if(newposition.getY()<oldposition.getY())
 				{
-					rootNode.rotate((float)0.03, 0, 0);
+					theObject.rotate((float)0.03, 0, 0);
 				}
 				else if(newposition.getY()>oldposition.getY())
 				{
-					rootNode.rotate((float)-0.03, 0, 0);
+					theObject.rotate((float)-0.03, 0, 0);
 				}		
 				oldposition = newposition.clone() ;
 			}
-			if(name.contentEquals("right"))
+		/*	if(name.contentEquals("right"))
 			{
 				rootNode.move((float)0.005, 0, 0);
 			}
@@ -95,7 +136,7 @@ public class Main extends SimpleApplication
 			else if(name.contentEquals("down"))
 			{
 				rootNode.move(0, (float)-0.005, 0);
-			}
+			}*/
 		}
 	};
 
